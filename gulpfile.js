a6dbe986d2d346bb4d03561b27b85ff2fe6d145d
marko <marko.bozic@protonmail.com>
2017-05-11 18:35:54 +0200

    admin master page setup

--- gulpfile.js
+++ gulpfile.js
@@ -12,5 +12,23 @@ var elixir = require('laravel-elixir');
  */
 
 elixir(function(mix) {
-    mix.sass('app.scss');
+    mix.sass('app.scss')
+
+        //kompajla css datoteke da se brže loadaju
+        .styles([
+            'libs/blog-post.css',
+            'libs/bootstrap.css',
+            'libs/font-awesome.css',
+            'libs/metisMenu.css',
+            'libs/sb-admin-2.css'
+        ], './public/css/libs.css')
+
+        //kompajla skripte da se berže izvode
+        .scripts([
+            'libs/jquery.js',
+            'libs/bootstrap.js',
+            'libs/metisMenu.js',
+            'libs/sb-admin-2.js',
+            'libs/scripts.js'
+        ], './public/js/libs.js')
 });
