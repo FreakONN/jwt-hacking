3ac7724f16b9897070e0cc00360280fb33807349
marko <marko.bozic@protonmail.com>
2017-05-11 14:28:03 +0200

    initial routing

--- app/User.php
+++ app/User.php
@@ -2,6 +2,7 @@
 
 namespace App;
 
+use function foo\func;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 
 class User extends Authenticatable
@@ -20,7 +21,18 @@ class User extends Authenticatable
      *
      * @var array
      */
+
     protected $hidden = [
         'password', 'remember_token',
     ];
+
+/*
+ * So, we can access the Phone model from our User.
+ * Now, let's define a relationship on the Phone model that will let us access the User that owns the phone.
+ * We can define the inverse of a hasOne relationship using the belongsTo method:
+*/
+    public function role(){
+        return $this->belongsTo('App\Role');
+    }
+
 }
